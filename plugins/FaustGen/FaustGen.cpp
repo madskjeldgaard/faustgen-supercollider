dbaec58ce33bb933b037b90d5169878d4eb7dbbf
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-06-25 16:37:26 +0200

    closer...

--- plugins/FaustGen/FaustGen.cpp
+++ plugins/FaustGen/FaustGen.cpp
@@ -12,18 +12,18 @@ namespace FaustGen {
 FaustGen::FaustGen() {
   // the Faust code to compile as a string (could be in a file too)
   std::string theCode = "import(\"stdfaust.lib\"); process = no.noise;";
-  std::string m_errorString = "did not work";
+  std::string m_errorString;
 
   // @fixme Not sure about this
-  auto optimize = -1;
+  auto optimize = -4;
 
   auto argc = 0;
-  const char **argv{};
+  const char **argv;
 
   // compiling in memory (createDSPFactoryFromFile could be used alternatively)
-  m_factory = createDSPFactoryFromString("faust", theCode, argc, argv, "",
+  const auto name = "faustgen";
+  m_factory = createDSPFactoryFromString(name, theCode, argc, argv, "",
                                          m_errorString, optimize);
-
   if (m_factory == nullptr) {
     auto errmsg = "Could not create FAUST dsp factory \n";
     Print(errmsg);
@@ -42,10 +42,16 @@ FaustGen::FaustGen() {
   // initializing the DSP instance with the SR
   m_dsp->init(static_cast<int>(sampleRate()));
 
-  // hypothetical audio callback, assuming m_input/m_output are previously
-  //   /* while (...) { */
-  /*   m_dsp->compute(128, m_input, m_output); */
-  /* } */
+  // Post debug info
+  Print("Created faust dsp instance \n");
+  Print("name:");
+  Print(name);
+  Print("\nnum inputs: \n");
+  Print("%d \n", m_dsp->getNumInputs());
+  Print("num outputs: \n");
+  Print("%d \n", m_dsp->getNumOutputs());
+  Print("samplerate: \n");
+  Print("%d \n", m_dsp->getSampleRate());
 
   mCalcFunc = make_calc_function<FaustGen, &FaustGen::next>();
   next(1);
@@ -59,13 +65,26 @@ FaustGen::~FaustGen() {
 }
 
 void FaustGen::next(int nSamples) {
-  const float *input = in(0);
+  const FAUSTFLOAT *input = in(0);
   float *outbuf = out(0);
 
-  // simple gain function
-  m_dsp->compute(nSamples, (FAUSTFLOAT **)input, (FAUSTFLOAT **)outbuf);
-}
+  FAUSTFLOAT **faustinputs{};
+  FAUSTFLOAT **faustoutputs{};
 
+  // Copy inputs to faust buffer
+  /* for (size_t i = 0; i < nSamples; i++) { */
+  /*   faustinputs[0][i] = input[i]; */
+  /* } */
+
+  // compute faust code
+  m_dsp->compute(nSamples, faustinputs, faustoutputs);
+
+  // Copy to output
+  for (size_t i = 0; i < nSamples; i++) {
+    Print("hello\n");
+    outbuf[i] = faustoutputs[0][i];
+  }
+}
 } // namespace FaustGen
 
 PluginLoad(FaustGenUGens) {
